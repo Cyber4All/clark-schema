67c2b2056e2cac2dca67f1cf23d52a5670a22c26
Gustavus Shaw II <gshaw1997@gmail.com>
2018-01-19 15:20:31 -0500

    added missing methods

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,63 @@
+import {
+    RecordID,
+    Record,
+    Insert,
+    Update,
+    Edit,
+    auto,
+    autosFor,
+    field,
+    fixed,
+    fixedsFor,
+    foreign,
+    foreignsFor,
+    fieldsFor,
+    collections,
+    collectionFor,
+    schemaFor,
+    textsFor,
+    text,
+    uniquesFor,
+    unique,
+    foreignData
+} from './db.schema';
+
 import { UserID, UserSchema, UserRecord, UserInsert, UserUpdate, UserEdit } from './user.schema';
 import {
     LearningObjectID, LearningObjectSchema, LearningObjectRecord,
     LearningObjectInsert, LearningObjectUpdate, LearningObjectEdit, LearningGoalInterface,
 } from './learning-object.schema';
-import { LearningOutcomeID, LearningOutcomeSchema, LearningOutcomeRecord,
+import {
+    LearningOutcomeID, LearningOutcomeSchema, LearningOutcomeRecord,
     LearningOutcomeInsert, LearningOutcomeUpdate, LearningOutcomeEdit,
 } from './learning-outcome.schema';
-import { StandardOutcomeID, StandardOutcomeSchema, StandardOutcomeRecord,
+import {
+    StandardOutcomeID, StandardOutcomeSchema, StandardOutcomeRecord,
     StandardOutcomeInsert, StandardOutcomeUpdate, StandardOutcomeEdit,
 } from './standard-outcome.schema';
-import { RecordID, Record, Insert, Update, Edit } from './db.schema';
 import { OutcomeID, OutcomeRecord, AssessmentPlanInterface, InstructionalStrategyInterface } from './outcome.schema';
 
 export {
+    auto,
+    autosFor,
+    field,
+    fixed,
+    fixedsFor,
+    foreign,
+    foreignsFor,
+    fieldsFor,
+    collections,
+    collectionFor,
+    schemaFor,
+    textsFor,
+    text,
+    uniquesFor,
+    unique,
+    foreignData,
     UserID, UserSchema, UserRecord, UserInsert, UserUpdate, UserEdit,
     LearningObjectID, LearningObjectSchema, LearningObjectRecord, LearningObjectInsert, LearningObjectUpdate, LearningObjectEdit,
     LearningOutcomeID, LearningOutcomeSchema, LearningOutcomeRecord, LearningOutcomeInsert, LearningOutcomeUpdate, LearningOutcomeEdit,
     StandardOutcomeID, StandardOutcomeSchema, StandardOutcomeRecord, StandardOutcomeInsert, StandardOutcomeUpdate, StandardOutcomeEdit,
     RecordID, Record, Insert, Update, Edit,
-    OutcomeID, OutcomeRecord, AssessmentPlanInterface, InstructionalStrategyInterface, LearningGoalInterface,
+    OutcomeID, OutcomeRecord, AssessmentPlanInterface, InstructionalStrategyInterface, LearningGoalInterface
 }
