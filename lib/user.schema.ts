95f53cd11c6ee52d3c2ec81a6a0b250ab8e6dcd7
Gustavus Shaw II <gshaw1997@gmail.com>
2018-09-17 13:16:07 -0400

    :sparkles: Add bio, createdAt, accessGroups properties  to UserDocument

--- lib/user.schema.ts
+++ lib/user.schema.ts
@@ -8,7 +8,10 @@ export type UserDocument = {
   name: string;
   email: string;
   organization: string;
+  bio: string;
   password: string;
   objects: string[];
   emailVerified: boolean;
+  createdAt: string;
+  accessGroups: string[];
 };
